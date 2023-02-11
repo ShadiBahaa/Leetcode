92c805c0da6a0774d76118fe524848bfcb5180ba
Shadi Bahaa <33010011+ShadiBahaa@users.noreply.github.com>
2023-02-11 23:17:19 +0200

    Time: 2851 ms (5.18%), Space: 13.8 MB (49.78%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        set<char> left;
+        map<char,int> right;
+        for (char x:s)right[x]++;
+        set<pair<char,char>> res;
+        for (char x:s){
+            if (right[x]>=1)right[x]--;
+            for (char c = 'a'; c<='z'; ++c){
+                if (left.count(c) && right[c])res.insert({x,c});
+            }
+            left.insert(x);
+        }
+        return res.size();
+    }
+};
