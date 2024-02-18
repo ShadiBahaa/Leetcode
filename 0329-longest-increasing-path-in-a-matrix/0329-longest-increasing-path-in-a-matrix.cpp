f83a80f483a96a5e21017a432f1fcae250c517c0
Shadi Bahaa <33010011+ShadiBahaa@users.noreply.github.com>
2024-02-18 19:59:00 +0200

    Time: 132 ms (14.77%), Space: 47.9 MB (8.81%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int dx[4] = {-1,1,0,0};
-    int dy[4] = {0,0,1,-1};
-    static bool check(int i, int j, int m, int n){
-        return i>=0 && j>=0 && i<m && j<n;
+    int dx[4] = {0,0,1,-1};
+    int dy[4] = {1,-1,0,0};
+    static bool is_valid(int& i, int& j, int& m, int& n){
+        return i >= 0 && i < m && j >= 0 && j<n;
     }
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        map<pair<int,int>,vector<pair<int,int>>> graph;
         int m = matrix.size();
         int n = matrix[0].size();
-        map<pair<int,int>,int> indegree;
+        map<pair<int,int>,vector<pair<int,int>>> graph;
+        vector<vector<int>> indegree(m,vector<int>(n));
         for (int i = 0; i<m; ++i){
             for (int j = 0; j<n; ++j){
-                for (int k = 0; k <4; ++k){
-                    int newx = i + dx[k];
-                    int newy = j + dy[k];
-                    if (check(newx,newy,m,n) && matrix[newx][newy]>matrix[i][j]){
-                        graph[{i,j}].push_back({newx,newy});
-                        indegree[{newx,newy}]++;
+                for (int k = 0; k<4; ++k){
+                    int newi = i + dx[k];
+                    int newj = j + dy[k];
+                    if (is_valid(newi,newj,m,n) && matrix[newi][newj]>matrix[i][j]){
+                        graph[{i,j}].push_back({newi,newj});
+                        indegree[newi][newj]++;
                     }
                 }
             }
@@ -25,25 +25,24 @@ public:
         queue<pair<int,int>> q;
         for (int i = 0; i<m; ++i){
             for (int j = 0; j<n; ++j){
-                if (!indegree[{i,j}]){
+                if (indegree[i][j]==0){
                     q.push({i,j});
                 }
             }
         }
-        int ans = 0;
+        int levels = 0;
         while (!q.empty()){
+            levels ++;
             int sz = q.size();
-            ans++;
             while (sz--){
                 pair<int,int> front = q.front();
                 q.pop();
-                for (pair<int,int> child: graph[front]){
-                    if (--indegree[child] == 0){
-                        q.push(child);
-                    }
+                for (pair<int,int> child : graph[front]){
+                    if (--indegree[child.first][child.second]==0)
+                    q.push(child);
                 }
             }
         }
-        return ans;
+        return levels;
     }
 };
