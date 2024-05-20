bc1869cc757df7eea04c5293f8781bbbae11e0fe
Shadi Bahaa <33010011+ShadiBahaa@users.noreply.github.com>
2024-05-20 20:58:18 +0300

    Time: 55 ms (43.72%), Space: 49.3 MB (10.70%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -4,5 +4,11 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    return arr.filter(fn);
+    const ans = [];
+    for (const index in arr){
+        if (fn(arr[index],Number(index))){
+            ans.push(arr[index]);
+        }
+    }
+    return ans;
 };
