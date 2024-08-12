1c51d9abb7541bc9f8196a147d3f1dcf5b8b7948
Shadi Bahaa <33010011+ShadiBahaa@users.noreply.github.com>
2024-08-12 21:22:18 +0300

    Time: 122 ms (84.56%), Space: 97.9 MB (74.79%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -8,11 +8,9 @@ public:
         if (mp[i][swapped]!=-1)return mp[i][swapped];
         int call1 = 1e9,call2 = 1e9;
         if (prev1 < num1[i] && prev2 < num2[i]){
-            //cout<<"yes 1"<<endl;
             call1 = dp(i+1,false,num1[i],num2[i]);
         }
         if (prev1<num2[i] && prev2<num1[i]){
-            //cout<<"yes 2"<<endl;
             call2 = 1 + dp(i+1,true,num2[i],num1[i]);
         }
         return mp[i][swapped] = min(call1,call2);
@@ -20,11 +18,30 @@ public:
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
         num1 = nums1;
         num2 = nums2;
-        for (int i = 0; i<100005; ++i){
+        /*for (int i = 0; i<100005; ++i){
             for (int j = 0; j<2; ++j){
                 mp[i][j] = -1;
             }
+        }*/
+        mp[num1.size()][0] = mp[num1.size()][1] =  0;
+        for (int i = num1.size()-1; i>=0; --i){
+            for (int swapped = 0; swapped<2; ++swapped){
+                int prev1 = ((i-1)<0)?-1:num1[i-1];
+                int prev2 = ((i-1)<0)?-1:num2[i-1];
+                if (swapped){
+                    swap(prev1,prev2);
+                }
+                int call1 = 1e9,call2 = 1e9;
+                if (prev1 < num1[i] && prev2 < num2[i]){
+                    call1 = mp[i+1][0];
+                }
+                if (prev1<num2[i] && prev2<num1[i]){
+                    call2 = 1 + mp[i+1][1];
+                }
+                mp[i][swapped] = min(call1,call2);
+            }
         }
-        return dp(0,false,-1,-1);
+        return mp[0][0];
+        //return dp(0,false,-1,-1);
     }
 };
