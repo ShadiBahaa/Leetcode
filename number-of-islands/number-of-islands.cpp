6ec0ed3c3e5a5fbcf3f0ab77c1c7734c926d08ba
Shadi Bahaa <33010011+ShadiBahaa@users.noreply.github.com>
2025-05-25 15:32:05 +0300

    Time: 102 ms, Memory: 29.4 MB - LeetHub

--- number-of-islands/number-of-islands.cpp
+++ number-of-islands/number-of-islands.cpp
@@ -2,32 +2,26 @@ class Solution {
 public:
     int dx[4] = {-1,1,0,0};
     int dy[4] = {0,0,1,-1};
-    map<pair<int,int>,bool> visited;
-    void bfs(vector<vector<char>>& grid, int i , int j){
-        queue<pair<int,int>> q;
-        q.push({i,j});
-        visited[{i,j}] = true;
-        while (!q.empty()){
-            pair<int,int> cur = q.front();
-            q.pop();
-            for(int k = 0; k<4; ++k){
-                int ni = cur.first + dx[k];
-                int nj = cur.second + dy[k];
-                if(ni<0 || ni>=grid.size())continue;
-                if(nj<0 || nj>=grid[0].size())continue;
-                if(!visited[{ni,nj}] && grid[ni][nj]=='1'){
-                    q.push({ni,nj});
-                    visited[{ni,nj}] = true;
-                }
+    map<pair<int,int>, bool> vis;
+    void dfs(vector<vector<char>>& grid, int i, int j){
+        vis[{i,j}] = true;
+        for (int k = 0; k<4; ++k){
+            int ni = i + dx[k];
+            int nj = j + dy[k];
+            if(ni < 0 || ni>=grid.size())continue;
+            if(nj < 0 || nj>=grid[0].size())continue;
+            if(!vis[{ni,nj}] && grid[ni][nj]=='1'){
+                dfs(grid,ni,nj);
             }
         }
     }
+    
     int numIslands(vector<vector<char>>& grid) {
         int count = 0;
-        for(int i = 0; i<grid.size(); ++i){
+        for (int i = 0; i<grid.size(); ++i){
             for (int j = 0; j<grid[i].size(); ++j){
-                if(grid[i][j]=='1' && !visited[{i,j}]){
-                    bfs(grid,i,j);
+                if(!vis[{i,j}] && grid[i][j]=='1'){
+                    dfs(grid,i,j);
                     count++;
                 }
             }
