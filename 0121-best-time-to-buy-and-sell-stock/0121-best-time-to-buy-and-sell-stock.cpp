b94545081425f38eadb09b042fce621072d2f940
Shadi Bahaa <33010011+ShadiBahaa@users.noreply.github.com>
2025-06-16 13:33:03 +0300

    Time: 2 ms (32.07%), Space: 97.2 MB (87.14%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        vector<int> maxi;
-        vector<int> mini;
-        int tmp = -1;
-        for (int i = (prices.size()-1); i>=0; --i){
-            maxi.push_back(max(prices[i],tmp));
-            tmp = max(prices[i],tmp);
+        int minStockValSoFar = 1e9, maxProfitValSoFar = -1;
+        for (int price : prices){
+            minStockValSoFar = min(minStockValSoFar,price);
+            maxProfitValSoFar = max(maxProfitValSoFar,price-minStockValSoFar);
         }
-        tmp = 1e9;
-        for (int i : prices){
-            mini.push_back(min(i,tmp));
-            tmp = min(i,tmp);
-        }
-        int dif = 0;
-        int i = prices.size()-1;
-        int j = 0;
-        while(i>=0 && j<=((int)prices.size()-1)){
-            dif = max(dif,maxi[i]-mini[j]);
-            i--; j++;
-        }
-        return dif;
+        return maxProfitValSoFar;
     }
 };
+/*
+
+[7,1,5,3,6,4]
+
+minStockValSoFar : 7, 1, 1, 1, 1, 1 
+
+maxProfitValSoFar: 0, 0, 4, 4, 5, 5
+
+*/
