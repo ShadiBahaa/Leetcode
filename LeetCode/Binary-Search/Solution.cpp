3fcbbf5e704212382a168c41345efa3f820b38dd
Shadi Bahaa <33010011+ShadiBahaa@users.noreply.github.com>
2025-08-10 17:06:22 +0300

    Add solution for Binary Search

--- LeetCode/Binary-Search/Solution.cpp
+++ LeetCode/Binary-Search/Solution.cpp
@@ -1,13 +1,17 @@
-def search(self, nums: List[int], target: int) -> int:
-    lo, hi = 0, len(nums) - 1
-    while lo < hi:
-        mid = (lo + hi) // 2
-        if nums[mid] >= target:
-            hi = mid
-        else:
-            lo = mid + 1
-
-    if nums[lo] == target:
-        return lo
-    else:
-        return -1
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() -1 ;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target)return mid;
+            if (nums[mid] < target){
+                low = mid + 1;
+            }else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
